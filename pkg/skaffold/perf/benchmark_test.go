e9923ccf39e2ad229bd3c1ba863dc4de9cf59936
Ilya Zuyev <izuyev@google.com>
2020-12-16 15:27:24 -0800

    add skaffold init benchmark

--- pkg/skaffold/perf/benchmark_test.go
+++ pkg/skaffold/perf/benchmark_test.go
@@ -1,13 +1,19 @@
 package perf
 
 import (
+	"context"
 	"flag"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
 
 	"github.com/docker/docker/pkg/ioutils"
+
+	sc "github.com/GoogleContainerTools/skaffold/pkg/skaffold/config"
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/initializer"
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/initializer/config"
 )
 
 var testProj = flag.String("target", "examples/getting-started", "The target skaffold project dir")
@@ -75,6 +81,48 @@ func BenchmarkDeploy(b *testing.B) {
 		err := cmd.Run()
 		if err != nil {
 			b.Errorf("failed to run skaffold: %v", err)
+		}
+	}
+}
+
+func BenchmarkFoo(b *testing.B) {
+	skRoot, err := filepath.Abs(*skDir)
+	if err != nil {
+		b.Fatalf("failed to process path: %v", err)
+	}
+	err = os.Chdir(filepath.Join(skRoot, *testProj))
+	if err != nil {
+		b.Fatalf("failed to chdir: %v", err)
+	}
+
+	tmpCfg, err := ioutil.TempFile("", "")
+	if err != nil {
+		b.Fatalf("failed to create temp file: %v", err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		cfg := config.Config{
+			ComposeFile:            "",
+			CliArtifacts:           []string{},
+			CliKubernetesManifests: []string{},
+			SkipBuild:              false,
+			SkipDeploy:             false,
+			Force:                  true,
+			Analyze:                false,
+			EnableJibInit:          false,
+			EnableJibGradleInit:    false,
+			EnableBuildpacksInit:   false,
+			EnableNewInitFormat:    false,
+
+			BuildpacksBuilder: "gcr.io/buildpacks/builder:v1",
+			Opts: sc.SkaffoldOptions{
+				Force:             true,
+				ConfigurationFile: tmpCfg.Name(),
+			},
+		}
+		err := initializer.DoInit(context.Background(), &ioutils.NopWriter{}, cfg)
+		if err != nil {
+			b.Errorf("failed to init skaffold: %v", err)
 		}
 	}
 }
